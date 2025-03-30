c1e47267782a72d5a744068716660bddbc4887ca
Nikita <54047002+Dmkls@users.noreply.github.com>
2025-03-31 01:15:25 +1000

    Update Contracts.py

--- utils/Contracts.py
+++ utils/Contracts.py
@@ -14,11 +14,11 @@ class Contracts:
 
     # Untitled bank
     # approve
-    __untitled_bank_approve_contract_address = AsyncWeb3.to_checksum_address("0x905108D47409068Bbd771f7a5d8a89AFDA94050D")
+    __untitled_bank_approve_contract_address = AsyncWeb3.to_checksum_address("0xbA9986D2381edf1DA03B0B9c1f8b00dc4AacC369")
     __untitled_bank_approve_abi = read_json("./data/abis/untitled_bank_abi_approve.json")
     untitled_bank_approve = w3.eth.contract(__untitled_bank_approve_contract_address, abi=__untitled_bank_approve_abi)
 
     # deposit
-    __untitled_bank_deposit_contract_address = AsyncWeb3.to_checksum_address("0x905108D47409068Bbd771f7a5d8a89AFDA94050D")
+    __untitled_bank_deposit_contract_address = AsyncWeb3.to_checksum_address("0x2469362f63e9f593087EBbb5AC395CA607B5842F")
     __untitled_bank_deposit_abi = read_json("./data/abis/untitled_bank_abi_deposit.json")
     untitled_bank_deposit = w3.eth.contract(__untitled_bank_deposit_contract_address, abi=__untitled_bank_deposit_abi)
